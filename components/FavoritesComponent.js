51259d52a5b5a24d6f6196e4b1813aa3a12bce4f
Ian De Jesus <ian.dejesus.fontilea@gmail.com>
2020-05-05 00:00:56 -0700

    Alert

--- components/FavoritesComponent.js
+++ components/FavoritesComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { FlatList, View, Text } from 'react-native';
+import { FlatList, View, Text, Alert } from 'react-native';
 import { ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { Loading } from './LoadingComponent';
@@ -34,7 +34,29 @@ class Favorites extends Component {
                 {
                     text: 'Delete',
                     type: 'delete',
-                    onPress: () => this.props.deleteFavorite(item.id)
+                    onPress: () => {
+                        Alert.alert(
+                            'Delete Favorite',
+                            'Are you sure yo uwish to delete the favorite campsite ' + item.name + '?',
+                            [
+                                {
+                                    text: 'Cancel',
+                                    onPress: () => console.log( item.name + ' Not Deleted'),
+                                    style: 'cancel'
+                                },
+                                {
+                                    text: 'Ok',
+                                    onPress: () => this.props.deleteFavorite(item.id)
+                                }
+                            ],
+                            {
+                                cancelable: false // alerts on andriod can canel when clicked outside the box
+                            }
+
+                        )
+                    } 
+                    
+                    
                 }
             ];
 
